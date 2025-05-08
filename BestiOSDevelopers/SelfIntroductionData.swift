3f18f1d88a4f7155840cd6afc5fcbbce96fbd904
Heejung Yang <wolla39@gmail.com>
2025-05-08 18:51:48 +0900

    자기소개 수정

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -351,7 +351,7 @@ let people: [SelfIntroduction] = [
     SelfIntroduction(
         name: "양희정",
         trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
+        hobby: "여행",
         mbti: "INTJ",
         strength: "끈기와 실행력",
         sns: nil,
