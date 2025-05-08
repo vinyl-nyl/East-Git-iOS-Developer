482f1006b668e143beda8652e9cc3db5518b8b50
박동언 <dongeon8943@gmail.com>
2025-05-08 17:14:35 +0900

    동언 소개 수정

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -1,7 +1,7 @@
 let people: [SelfIntroduction] = [
     SelfIntroduction(
         name: "최정은",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
+        trainingPurpose: "iOS 강의 매니저님",
         hobby: "음악 감상, 여행, 독서 등",
         mbti: "INTJ",
         strength: "끈기와 실행력",
@@ -280,13 +280,13 @@ let people: [SelfIntroduction] = [
         job: "쿠팡 - iOS 개발자"
     ),
     SelfIntroduction(
-        name: "박동언",
+        name: "박동언 (수정됨)",
         trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "운동, 음악 듣기",
-        mbti: "ESTJ",
-        strength: "끈기와 실행력",
+        hobby: "운동, 음악 듣기 (수정됨)",
+        mbti: "ESTJ (수정됨)",
+        strength: "끈기와 실행력 (수정됨)",
         sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
+        etc: "열심히 수료하고 취업하겠습니다! (수정됨)",
         job: "쿠팡 - iOS 개발자"
     ),
     SelfIntroduction(
